bfb4d0ffa6402ca718064309c93f08c32d05b60e
Manprabhnoor Kaur <harhome2121@gmail.com>
2026-08-12 02:03:08 +0530

    startedJS

---
+++ mydaywiselearning/day1.js
@@ -0,0 +1,6 @@
+const button=document.querySelector('button'); 
+button.addEventListener('click', () => {
+    alert('Button clicked!');
+});
+//querySelector helps us search the query in the whole document
+//document is said to be as the current webpage
