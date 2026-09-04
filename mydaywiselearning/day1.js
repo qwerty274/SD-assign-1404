282cbc02dc76c271b3d80fb99043e15ad3090944
Manprabhnoor Kaur <harhome2121@gmail.com>
2026-09-04 20:38:33 +0530

    Reorganize certificate generator project

--- mydaywiselearning/day1.js
+++
@@ -1,6 +0,0 @@
-const button=document.querySelector('button'); 
-button.addEventListener('click', () => {
-    alert('Button clicked!');
-});
-//querySelector helps us search the query in the whole document
-//document is said to be as the current webpage
--- todo.js
+++
@@ -1,4 +0,0 @@
-function addTask(){
-    document.getElementById('myInput').value = "";
-    document.getElementById('myUL').innerHTML += "<li>" + document.getElementById('myInput').value + "</li>";
-}
